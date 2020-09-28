17751d5cc1223437990cbd1c96ef50d04527e105
Zoe <hi@zoe.im>
2020-09-28 22:29:10 +0800

    :tada: reinstall chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from "react"
-import { Switch, Route, BrowserRouter, withRouter } from "react-router-dom"
-import { inject, observer } from "mobx-react"
+import React from "react";
+import { Switch, Route, BrowserRouter, withRouter } from "react-router-dom";
+import { inject, observer } from "mobx-react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/core";
 
-import { Flex, Button } from "@chakra-ui/core"
+import theme from "./styles/theme";
 
 @withRouter
 @inject("appStore")
 @observer
 export default class App extends React.Component {
-
     _onClick() {
-        this.props.appStore.addCount()
+        this.props.appStore.addCount();
     }
 
     render() {
-        return <Flex>
-            Hello {this.props.appStore.count} Chakra UI!
-            <Button onClick={this._onClick.bind(this)}>Add</Button>
-        </Flex>
+        // use register layout
+        return (
+            <ChakraProvider theme={theme}>
+                <CSSReset />
+            </ChakraProvider>
+        );
     }
 }

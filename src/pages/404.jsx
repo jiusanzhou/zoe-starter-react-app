cb44a3144493b2af3ccfceff6833a67f9bbcae32
Zoe <hi@zoe.im>
2020-10-09 09:54:30 +0800

    :tada: add 404 box

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { Center, Flex } from '@chakra-ui/core';
+import {
+  Center,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Box,
+  Code,
+} from '@chakra-ui/core';
 
-export default (props) => {
-    console.log("====>", props)
-    return <Flex w="100%">
-    <Center>404</Center>
-</Flex>;
-}
+export default ({ location, history, title = '404：页面未找到!' }) => {
+  return (
+    <Center w="100%">
+      <Flex maxWidth="80em" p="1rem" margin="auto">
+        <Box w="sm" borderWidth="1px" rounded="lg" p='1rem'>
+          <Heading size="sm">{title}</Heading>
+          <Text mt="1em">
+            URL: <Code>{location.pathname}</Code>
+          </Text>
+          <Flex mt="1em" justifyContent="end">
+            <Button
+              size="sm"
+              onClick={() => {
+                history.goBack();
+              }}
+            >
+              返回
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </Center>
+  );
+};
